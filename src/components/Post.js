45dc3bd6426456d08680fe6ae55c71d9c1f8c20b
Prabhjyot Singh <prabhjyotg@gmail.com>
2021-12-08 00:25:36 +0000

    TESTING

--- src/components/Post.js
+++ src/components/Post.js
@@ -29,6 +29,18 @@ function Post({ displayName, username, verified, text, image, avatar }) {
           </div>
         </div>
         <img src="iph.jpg" alt=""></img>
+        <div className="post__headerDescription">
+          <p>
+            This is a text for testing.Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </div>
         <div className="post__footer">
           <ChatBubbleOutlineIcon fontSize="small" />
           <RepeatIcon fontSize="small" />
