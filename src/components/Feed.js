5faa72f9c49a855cc1cc176006c7da6377f0e0d8
Prabhjyot Singh <prabhjyotg@gmail.com>
2021-12-08 01:10:55 +0000

    TESTING 4

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 import "../views/Feed.css";
 import PostBox from "./PostBox.js";
 import Post from "./Post.js";
 
-function Feed(){
-    return <div className="feed">
-        {/*Header*/}
-        <div className="feed__header">
+function Feed() {
+  return (
+    <div className="feed">
+      {/*Header*/}
+      <div className="feed__header">
         <h2>Home</h2>
-        </div>
-        {/*PostBox*/}
-        <PostBox></PostBox>
-        {/*Post */}
-        <Post></Post>
+      </div>
+      {/*PostBox*/}
+      <PostBox></PostBox>
+      {/*Post */}
+      <Post></Post>
+      <Post></Post>
     </div>
+  );
 }
 
-export default Feed
+export default Feed;
